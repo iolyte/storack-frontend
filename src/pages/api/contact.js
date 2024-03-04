135c9baffee2dd91837368ee3930dda11c22d933
Darpit_Makwana <darpitmakwana@gmail.com>
2024-03-04 10:23:21 +0530

    Add contact form submission functionality

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,6 +1,33 @@
+import Axios from 'axios';
+const base_url = process.env.NEXT_PUBLIC_BASE_URL;
+
 export const ContactDetails = {
   telephone: '123-456-7890',
   email: 'support.storack@gmail.com',
   address:
     '2nd Floor, 1st Building, No. 188, East Road, Ahmadabad, Gujarat, India',
+};
+
+export const contactUsForm = async (bodyData) => {
+  let res = {
+    status: 0,
+    message: '',
+  };
+
+  await Axios.post(`${base_url}/pub/contact-us`, bodyData).then(
+    (response) => {
+      res = {
+        status: response.code,
+        message: response.data.message,
+      };
+    },
+    (error) => {
+      res = {
+        status: error.code,
+        message: error.message || error,
+      };
+      console.log(error);
+    }
+  );
+  return res;
 };
